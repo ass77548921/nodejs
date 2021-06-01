d7888f36b3371309784d85e0443ab93ac10f98f2
ass77548921 <a.smart.andrew@gmail.com>
2021-06-01 22:40:55 +0800

    Initial commit

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -16,18 +16,38 @@ module.exports = {
     },
 
 
-    categoryPage: async (ctx) => {
+   categoryPage: async (ctx) => {
         const category = ctx.params.category;
-        console.log("category",category);
-        const cat =await strapi.services.category.findOne({name: category});
-        console.log('cat',category,cat.cid);
-        const data =await strapi.connections.default.raw(`SELECT * FROM products where category = ${cat.id}`);
-        console.log('data',JSON.stringify(data));
+        //console.log("category",category);
+        const cat = await strapi.services.category.findOne({name: category});
+        //console.log('cat',category,cat.cid);
+        const data = await strapi.connections.default.raw(`SELECT * FROM products where category = ${cat.id}`);
+        //console.log('data',JSON.stringify(data));
         return await ctx.render("products_xx",{
             data,
             title: "shop products",
         });
     },
+
+    overviewPage: async (ctx) => {
+        let data = {};
+        try{
+            const entities = await strapi.services.category.find();
+            data.hats = entities[0].products;
+            data.jackets = entities[1].products; 
+            data.sneakers = entities[2].products;
+            data.womens = entities[3].products;
+            data.mens = entities[4].products;
+            
+            return await ctx.render("overview",{
+                data,
+                title: "shop overview",
+                count: 4
+            });
+        }catch(err){
+            console.log(err);
+        }
+    }
     
     
 };
